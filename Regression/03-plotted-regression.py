2ddf4e5ba2d5d1f3e2abc45d750cf3ed6cd86769
elham zeinodini <53154378+elhamzeinodini@users.noreply.github.com>
2024-10-06 09:36:16 +0330

    Update 03-plotted-regression.py

--- Regression/03-plotted-regression.py
+++ Regression/03-plotted-regression.py
@@ -9,16 +9,15 @@ y = -15 * x + 20 + noise
 
 X = np.sum(x)
 Y = np.sum(y)
-b1 = (n * np.dot(x,y) - Y*X)/(n*np.dot(x,x)-X**2)
-b0 = (Y-b1*X)/n
+b1 = (n * np.dot(x, y) - Y * X) / (n * np.dot(x, x) - X ** 2)
+b0 = (Y - b1 * X) / n
 b0, b1 = round(b0, 2), round(b1, 2)
 print(f"b0 = {b0} x + {b1}")
 
 def MSE_Loss(b0, b1, x, y):
-  predict = b0 + b1 * x
-  print(f"predict= {predict}")
-  loss = np.sum((predict - y) ** 2) / len(x)
-  return loss
+    predict = b0 + b1 * x
+    loss = np.sum((predict - y) ** 2) / len(x)
+    return loss
 
 # y = -15 x + 20
 # w0 = b0 = 20
@@ -27,18 +26,18 @@ b0_vals = np.linspace(0, 40, 100)
 # w1 = b1 = -15
 b1_vals = np.linspace(-30, 0, 100)
 
-
-# 2d array of zeros
+# 2D array of zeros
 z = np.zeros((len(b0_vals), len(b1_vals)))
 
-for i in range(len(x)) :
-  for j in range(len(y)):
-    # logarithm is for better displaying the data
-    z[i, j] = np.log(MSE_Loss(b0_vals[i], b1_vals[j],x, y))
-    print(f"{z[i, j]}")
+# Populate the z array with MSE loss values
+for i in range(len(b0_vals)):
+    for j in range(len(b1_vals)):
+        # Logarithm is for better displaying the data
+        z[i, j] = np.log(MSE_Loss(b0_vals[i], b1_vals[j], x, y))
 
-
-fig = plt.figure(figsize=(10, 7))
+# Plotting the surface
+# a figure with 10 inches width and 20 inches height
+fig = plt.figure(figsize=(10, 20))
 ax = fig.add_subplot(111, projection='3d')
 B0, B1 = np.meshgrid(b0_vals, b1_vals)
 ax.plot_surface(B0, B1, z, cmap='viridis')
