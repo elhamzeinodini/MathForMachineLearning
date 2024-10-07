58a3605d08d9a45ff5ec9615a666d6eba3be591b
elham zeinodini <53154378+elhamzeinodini@users.noreply.github.com>
2024-10-07 09:11:32 +0330

    Update 03-plotted-regression.py

--- Regression/03-plotted-regression.py
+++ Regression/03-plotted-regression.py
@@ -2,48 +2,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+# feature count
 n = 1000
-noise = np.random.normal(0, 3, size=n)
+
+# input
 x = np.random.normal(3, 5, size=n)
+
+# output
+noise = np.random.normal(0, 3, size=n)
 y = -15 * x + 20 + noise
 
-X = np.sum(x)
-Y = np.sum(y)
-b1 = (n * np.dot(x, y) - Y * X) / (n * np.dot(x, x) - X ** 2)
+X = np.sum(x) # an scalar
+Y = np.sum(y) # an scalar
+
+b1 = (n * np.dot(x, y) - X * Y) / (n * np.dot(x, x) - X ** 2)
 b0 = (Y - b1 * X) / n
-b0, b1 = round(b0, 2), round(b1, 2)
-print(f"b0 = {b0} x + {b1}")
 
-def MSE_Loss(b0, b1, x, y):
-    predict = b0 + b1 * x
-    loss = np.sum((predict - y) ** 2) / len(x)
-    return loss
 
-# y = -15 x + 20
-# w0 = b0 = 20
+# MEAN SQUARED ERROR
+def MSE_LOSS(b0, b1, x, y):
+  predict = b0 + b1 * x
+  loss = np.sum((predict - y) ** 2) / len(x)
+  return loss
+
+# b0 = w0 = 20, 100 numbers in range of 0 to 40
 b0_vals = np.linspace(0, 40, 100)
 
-# w1 = b1 = -15
+# b1 = w1 = -15, 100 numbers in range of -30 to 0
 b1_vals = np.linspace(-30, 0, 100)
 
-# 2D array of zeros
+# 2d array of zeros
 z = np.zeros((len(b0_vals), len(b1_vals)))
 
-# Populate the z array with MSE loss values
-for i in range(len(b0_vals)):
-    for j in range(len(b1_vals)):
-        # Logarithm is for better displaying the data
-        z[i, j] = np.log(MSE_Loss(b0_vals[i], b1_vals[j], x, y))
+for i in range(len(b0_vals)) :
+  for j in range(len(b1_vals)) :
+    z[i, j] = np.log(MSE_LOSS(b0_vals[i], b1_vals[j], x, y))
 
-# Plotting the surface
-# a figure with 10 inches width and 20 inches height
-fig = plt.figure(figsize=(10, 20))
+
+# a figure of "width=10" and "height=7"
+fig = plt.figure(figsize=(10, 7))
 ax = fig.add_subplot(111, projection='3d')
 B0, B1 = np.meshgrid(b0_vals, b1_vals)
 ax.plot_surface(B0, B1, z, cmap='viridis')
 
+
 ax.set_xlabel('b0')
 ax.set_ylabel('b1')
-ax.set_zlabel('Loss')
+ax.set_zlabel('loss')
 
 plt.show()
+
+# this figure shows that at (b0=w0=-20) and (b1=w1=-15) we have the lowest amount of error and from any point on this 
+# figure if we try to move in direction of decreasing the error , we'll eventually get to this point (b0=w0=-20) and (b1=w1=-15) again.
